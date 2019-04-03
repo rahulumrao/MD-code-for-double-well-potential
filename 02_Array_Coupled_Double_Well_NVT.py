832e57172da2d5fff773a19aa07a650e2db118f1
Rahul <vrahul@Umrao>
2019-04-03 18:14:23 +0530

    first commit

--- 02_Array_Coupled_Double_Well_NVT.py
+++ 02_Array_Coupled_Double_Well_NVT.py
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------------------------------------------------------------------------
 ''' THIS PROGRAM IS WRITTEN IN PYTHON3 TWO SOLVE THE PROBLEM OF DOUBLE WELL POTENTIAL WHICH IS HARMONIC
 IN y-axis AND HAS A BRRIER OF 0.01au IN x-axis WITH THE TWO MINIMUM AT +1.5 AND -1.5
-{U = 0.01*(x**2 - a**2)**2 - 8.5*k*y**2}
+{U = 0.01*(x**2 - a**2)**2 - 0.5*k*y**2}
 REQUIRED LIBRARY FOR THIS POROGRAM IS "numpy, math and matplotlib(IF YOU WANT TO PLOT USING PYTHON).
 %% WRITTEN IN APRIL 3rd 2019 in SL302 by RAHUL VERMA %% '''
 #----------------------------------------------------------------------------------------------------------------------------------------------
